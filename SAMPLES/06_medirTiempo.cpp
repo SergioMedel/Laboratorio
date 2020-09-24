ead0c12f99608e9b50a0dc59e0e32e46ee056452
Alexander Espina-Leyton <al.espina@uandresbello.edu>
2020-09-24 13:29:40 -0300

    modificado el tomar tiempo sin sleep

--- SAMPLES/06_medirTiempo.cpp
+++ SAMPLES/06_medirTiempo.cpp
@@ -5,11 +5,18 @@ using namespace std;
 
 int main ()
 {
+  int d = 0;
   clock_t t;
+  time_t f;
   cout << "Iniciando Reloj..." << endl;
   t = clock();
-  sleep(0);
+  f = time(0); //fecha
+  //sleep(3); //cantidad de segundos a esperar
+  //duerme completamente el proceso así que no contará el tiempo
+  for(int i = 0; i < CLOCKS_PER_SEC*64*10 ; i++){
+    d++;
+  }
   t = clock() - t;
-  cout << ((float)t) << " segundos." << endl;
+  cout << ((float)t/CLOCKS_PER_SEC) << " segundos, el valor d = " << d << " y comenzó el día " << ctime(&f) << endl;
   return 0;
 }
